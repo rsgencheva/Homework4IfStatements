90c75d2c220ae425a2d2ae77e59d3c61970bc024
RadostinaGencheva <RadostinaGencheva@WPF2YPFDA>
2025-11-23 23:10:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AgeValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            do
+            {
+                Console.WriteLine("Enter valid age");
+            }while (!int.TryParse(Console.ReadLine(), out age));
+
+            if(age < 0 || age > 120)
+            {
+                Console.WriteLine("This is not a valid age for person");
+            }else if(age < 18)
+            {
+                Console.WriteLine("The person is a minor");
+            }else 
+            {
+                Console.WriteLine("The person is an adult");
+            }
+        }
+    }
+}
